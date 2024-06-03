d59faae66908e705aa627297b2c327530cc17fa6
Deeksha Singhai <132802902+deekshasinghai@users.noreply.github.com>
2024-06-03 11:35:35 +0530

    Create ID 10366-customer-feedback-analysis.sql

---
+++ ID 10366-customer-feedback-analysis.sql
@@ -0,0 +1,10 @@
+-- Capital One's marketing team is working on a project to analyze customer feedback from their feedback surveys.
+--The team sorted the words from the feedback into three different categories
+--short_comments
+--mid_length_comments
+--long_comments
+--The team wants to find comments that are not short and that come from social media. The output should include 'feedback_id,' 'feedback_text,' 'source_channel,' and a calculated category.
+
+Query:
+select * from customer_feedback
+where comment_category != 'short_comments' and source_channel = 'social_media'
